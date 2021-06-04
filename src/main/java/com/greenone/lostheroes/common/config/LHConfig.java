b00d20113c4b51fca55d263ea8c3be2f986bc880
BuildTools <unconfigured@null.spigotmc.org>
2021-06-04 11:46:27 -0500

    Minor Mana HUD Tweaks

--- src/main/java/com/greenone/lostheroes/common/config/LHConfig.java
+++ src/main/java/com/greenone/lostheroes/common/config/LHConfig.java
@@ -4,12 +4,14 @@ import net.minecraftforge.common.ForgeConfigSpec;
 
 public class LHConfig {
     private static ForgeConfigSpec.DoubleValue manaHUDScale;
+    private static ForgeConfigSpec.BooleanValue leftHUD;
     private static ForgeConfigSpec.IntValue baseMaxMana;
     private static ForgeConfigSpec.IntValue hadesCooldown;
 
     public static void init(ForgeConfigSpec.Builder server, ForgeConfigSpec.Builder client){
         server.comment("LostHeroes server side config").push("server");
         manaHUDScale = server.comment("Scale of Mana HUD: ").defineInRange("manaHUDScale", 0.4D,0.1D, 0.7D);
+        leftHUD = server.comment("HUD on the Left Side: ").define("leftHUD", false);
         baseMaxMana = server.comment("Default Max Mana: ").defineInRange("baseMaxMana", 10, 5, 30);
         hadesCooldown = server.comment("Hades Second-Life Cooldown(ticks): ").defineInRange("hadesCooldown", 36000, 0, 36000);
         server.pop();
@@ -25,5 +27,9 @@ public class LHConfig {
 
     public static int getHadesCooldown() {
         return hadesCooldown.get();
+    }
+
+    public static boolean getLeftHUD() {
+        return leftHUD.get();
     }
 }

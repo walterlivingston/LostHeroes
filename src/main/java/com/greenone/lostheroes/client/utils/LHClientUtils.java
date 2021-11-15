7f8bb9a4743627bff5a2ce88b7b0e81eccb954b2
Walter Livingston <wblivingston18@gmail.com>
2021-11-15 16:42:59 -0600

    Fixed Ares Vignette not Rendering

--- src/main/java/com/greenone/lostheroes/client/utils/LHClientUtils.java
+++ src/main/java/com/greenone/lostheroes/client/utils/LHClientUtils.java
@@ -1,5 +1,6 @@
 package com.greenone.lostheroes.client.utils;
 
+import com.greenone.lostheroes.LostHeroes;
 import com.greenone.lostheroes.client.gui.ManaHUD;
 import com.greenone.lostheroes.common.capabilities.CapabilityRegistry;
 import com.greenone.lostheroes.common.capabilities.IPlayerCap;
@@ -9,6 +10,7 @@ import com.greenone.lostheroes.common.network.PacketAbility;
 import com.greenone.lostheroes.common.potions.LHEffects;
 import com.greenone.lostheroes.common.util.LHUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.IngameGui;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.InputEvent;
@@ -42,12 +44,13 @@ public class LHClientUtils {
     @SubscribeEvent
     public void onRenderPost(RenderGameOverlayEvent.Post event){
         if(event.getType() == RenderGameOverlayEvent.ElementType.VIGNETTE){
-            if(Minecraft.getInstance().player.hasEffect(LHEffects.RAGE)){
-                LHUtils.renderRageOverlay();
-            }
+
         }
         if(event.getType() == RenderGameOverlayEvent.ElementType.EXPERIENCE){
             new ManaHUD(Minecraft.getInstance()).render(event.getMatrixStack(), (float) LHConfig.getHUDScale());
+            if(Minecraft.getInstance().player.hasEffect(LHEffects.RAGE)){
+                LHUtils.renderRageOverlay();
+            }
         }
     }
 }
